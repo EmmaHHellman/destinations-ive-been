d9c5d2d5845b844493d6e2f7203eba703a8e4d17
Janet <janetkarpenske@gmail.com>
2020-06-15 14:19:41 -0700

    Add text to html file to create a form which asks the user for input and a section to reveal a list of inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,52 @@
-function Destination (location, landmarks, timeOfYear, notes) {
-  this.location = location;
-  this.landmarks = landmarks;
-  this.timeOfYear = timeOfYear;
-  this.notes = notes;
+function Travels() {
+ this.destinations = [];
+ this.currentid = 0;
 }
 
-let mthood = new Destination("Mt. Hood, Oregon", "Timberline Lodge", "Winter", "We went skiing and sledding.");
-console.log(mthood)
+Travels.prototype.addDestination = function(destination) {
+  destination.id = this.assignId();
+  this.destinations.push(destination);
+}
+
+Travels.prototype.assignId = function () {
+  this.currentid += 1;
+  return this.currentId;
+}
+
+Travels.prototype.findDestination = function(id) {
+  for(let i=0; i<this.destinations.length; i++) {
+  if (this.destinations[i]) {
+    if(this.destinations[i].id == id) {
+    return this.destinations[i];
+    }
+  }
+};
+    return false;
+}
+
+Travels.prototype.deleteContact = function(id) {
+  for(let i=0; i<this.destinations.length; i++) {
+    if(this.destinations[i]) {
+      if(this.destinations[i].id == id) {
+        delete this.destinations[i];
+        return true;
+        }
+      }
+    };
+    return false; 
+}
+
+function Destination(location, landmark, timeOfYear, activities) { 
+  this.location = location;
+  this.landmark = landmark;
+  this.timeOfYear = timeOfYear;
+  this.activity = activities;
+}
+
+Destination.prototype.fullList = function() {
+  return this.location + " " + this.landmark + " " + this.timeOfYear + " " + this.activity;
+}
+
+$(document).ready(function(){
+  
+})
